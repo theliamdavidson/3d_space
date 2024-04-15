32fd239cf83757c80fbb34213e3e9b8fd0488ed9
theliamdavidson <misterliamd@gmail.com>
2024-04-15 11:39:25 -0700

    first commit

---
+++ MMA8451_test_XYZ_y_acceleration.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+static int loop_counter = 0;
+
+void setup(void) {
+  Serial.begin(9600);
+  
+  Serial.println("start test");
+  
+
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  
+  mma.setRange(MMA8451_RANGE_2_G);
+  
+  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+  Serial.println("G");
+}
+
+int find_avg(int valueArray[]){       // we know the size of the array so passing it is simple
+  int avg = 0;
+  for(int i = 0; i < 4; i++){
+    //Serial.print(valueArray[i]);Serial.print(" ");
+    avg += valueArray[i];
+  }
+  avg = avg/4.0;
+  avg += 0.5;                         // rounding up the number by adding .5 and flooring the result
+  avg = int(avg);
+  return avg;
+}
+
+void loop() {
+  // Read the 'raw' data in 14-bit counts
+  int myX[4];
+  //int myXr[4];
+  int myY[4];
+  //int myYr[4];
+  int myZ[4];
+  //int myZr[4];
+  while (loop_counter < 4){
+    //mma.read();
+    //myXr[loop_counter] = mma.x;
+    //myYr[loop_counter] = mma.y;
+    //myZr[loop_counter] = mma.z;
+  
+    /* Get a new sensor event */ 
+    sensors_event_t event; 
+    mma.getEvent(&event);
+
+    myX[loop_counter] = event.acceleration.x;
+    myY[loop_counter] = event.acceleration.y;
+    myZ[loop_counter] = event.acceleration.z;
+
+  
+    //acceleration is measured in m/s^2 
+  
+    //average 4 value reads
+  
+    delay(50);
+    loop_counter++;
+  }
+  loop_counter = 0;
+  Serial.print("x:");
+  Serial.println(find_avg(myX));
+  Serial.print("y:");
+  Serial.println(find_avg(myY));
+  Serial.print("z:");
+  Serial.println(find_avg(myZ));
+  
+}
---
+++ read_acc.py
@@ -0,0 +1,78 @@
+import serial
+import sys
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QLabel
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtGui import QFont
+from queue import Queue
+from time import sleep
+
+class DeviceCommunicationThread(QThread):
+    # this class is on hold until telemetry is finished
+    # modify as needed
+    measurements_updated = pyqtSignal(dict)
+    def __init__(self, queue):
+        super().__init__()
+        self.modbus_data_queue = queue
+
+    def run(self):
+        with serial.Serial('COM5', 9600, timeout=1) as ser:
+            ser.write(b'HANDSHAKE_REQUESTED\n')
+            while not self.isInterruptionRequested():
+                received_values = {"x" : None,
+                                   "y" : None,
+                                   "z" : None}
+                count = 0
+                while count < 3:
+                    response = ser.readline().decode().strip()
+                    print(response)
+
+                    values = response.split(':')
+                    if values[0] in received_values:
+                        received_values[values[0]] = values[1]
+                        count += 1
+                    print(count)
+                    
+
+                #print(received_values)
+                self.measurements_updated.emit(received_values)
+                
+
+
+class viewWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        # Create the communication thread
+        telemetry_queue = Queue()        
+        self.communication_thread = DeviceCommunicationThread(telemetry_queue)
+        self.communication_thread.measurements_updated.connect(self.updateData)
+        self.communication_thread.start()
+
+    def initUI(self):
+        self.setWindowTitle("IMU Readings")
+        self.setGeometry(10,30,1890,1080)
+        layout = QHBoxLayout(self)
+        self.value_labels= {}
+        measurements = [
+            "x", "y", "z"           
+        ]
+        for measurement in measurements:
+            value_label = QLabel("Connecting...", alignment=Qt.AlignCenter)    # if you have time to read "Connecting", something is wrong with the connection
+            value_label.setFont(QFont('Arial', 20))
+            layout.addWidget(value_label)
+            self.value_labels[measurement] = value_label
+        self.setLayout(layout)
+        self.show()
+    
+    def updateData(self, values):
+        print("------")
+        for label, labelIndex in self.value_labels.items():
+            value = values[label]
+            labelIndex.setText(f'{label}: {value}')#:.2f}') these won't be a float unless we do tens shifting from the plc
+        print("------")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = viewWindow()
+    sys.exit(app.exec_())
