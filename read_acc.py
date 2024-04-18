d4357988fd807b79addce4b92153c0445b76daf2
theliamdavidson <misterliamd@gmail.com>
2024-04-18 07:41:59 -0700

    adding in visual feedback

--- read_acc.py
+++ read_acc.py
@@ -1,7 +1,7 @@
 import serial
 import sys
 import pyqtgraph as pg
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QProgressBar
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QFont
 from time import sleep
@@ -10,24 +10,38 @@ class DeviceCommunicationThread(QThread):
     measurements_updated = pyqtSignal(dict)
     def __init__(self, ):
         super().__init__()
+        self.comPort = 'COM8'
 
     def run(self):
-        with serial.Serial('COM5', 9600, timeout=1) as ser:
-            while not self.isInterruptionRequested():
-                received_values = {"x" : None,
-                                   "y" : None,
-                                   "z" : None}
-                count = 0
-                while count < 3:
-                    response = ser.readline().decode().strip()
-                    values = response.split(':')
-                    if values[0] in received_values:
-                        received_values[values[0]] = values[1]
-                        count += 1                    
-                self.measurements_updated.emit(received_values)
-                
+        try:
+            self.communicateWithIMU()
+        except serial.serialutil.SerialException:
+            for index in range(1,13):
+                self.searchForCom(index)
 
-class viewWindow(QWidget):
+    def communicateWithIMU(self):
+        with serial.Serial(self.comPort, 9600, timeout=1) as ser:
+                while not self.isInterruptionRequested():
+                    received_values = {"x" : None,
+                                    "y" : None,
+                                    "z" : None}
+                    count = 0
+                    while count < 3:
+                        response = ser.readline().decode().strip()
+                        values = response.split(':')
+                        if values[0] in received_values:
+                            received_values[values[0]] = values[1]
+                            count += 1                    
+                    self.measurements_updated.emit(received_values)
+
+    def searchForCom(self, index):
+        try:
+            with serial.Serial(self.comPort, 9600, timeout=1) as ser:
+                print(ser.readline().decode().strip())
+        except serial.serialutil.SerialException:
+            pass
+
+class ViewWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -39,36 +53,45 @@ class viewWindow(QWidget):
                              "y" : None,
                              "z" : None}
         self.lock = True
-        self.valueSaved = False
+
     def keyPressEvent(self, event):
        if event.key() == Qt.Key_Space:
            self.freezeCalled()
 
     def initUI(self):
         self.setWindowTitle("IMU Readings")
-        self.setGeometry(10,30,1890,1080)
+        #self.setGeometry(10,30,1890,1080)
         layout = QVBoxLayout(self)
         rawDataHorizontal = QHBoxLayout(self)
         savedDataHorizontal = QHBoxLayout(self)
-        self.rawValueLabels= {}
+        progressBarHorizontal = QHBoxLayout(self)
+        self.rawValueLabels = {}
         self.savedValueLabels = {}
+        self.progressBars = {}
         measurementLabels = [
             "x", "y", "z"           
         ]
         
         for measurement in measurementLabels:
             value_label = QLabel("Connecting...", alignment=Qt.AlignCenter)    # if "Connecting" remains for more than 10 seconds, something is wrong with the connection
-            value_label.setFont(QFont('Arial', 20))
-            rawDataHorizontal.addWidget(value_label)
-            self.rawValueLabels[measurement] = value_label
-        layout.addLayout(rawDataHorizontal)
+            savedValueLabel = QLabel("", alignment=Qt.AlignCenter)
+            visualAid = QProgressBar()
 
-        for label in measurementLabels:
-            savedValueLabel = QLabel("", alignment=Qt.AlignCenter)    
+            value_label.setFont(QFont('Arial', 20))
             savedValueLabel.setFont(QFont('Arial', 20))
+            visualAid.setRange(0, 100)
+
+            rawDataHorizontal.addWidget(value_label)
             savedDataHorizontal.addWidget(savedValueLabel)
-            self.savedValueLabels[label] = savedValueLabel
+            progressBarHorizontal.addWidget(visualAid)
+
+            self.rawValueLabels[measurement] = value_label
+            self.savedValueLabels[measurement] = savedValueLabel
+            self.progressBars[measurement] = visualAid
+
+        layout.addLayout(rawDataHorizontal)            
         layout.addLayout(savedDataHorizontal)
+        layout.addLayout(progressBarHorizontal)
         
         freeze = QPushButton(f'Freeze', self)
         freeze.clicked.connect(self.freezeCalled)
@@ -78,28 +101,35 @@ class viewWindow(QWidget):
         self.show()
     
     def updateData(self, values):
-        while self.lock == False:
-            sleep(.1)
-            print("wait")
+        #while self.lock == False:
+         #   sleep(.1)
+        #    print("wait")
 
         self.lock = False
         for label, labelIndex in self.rawValueLabels.items():
             value = values[label]
             labelIndex.setText(f'{label}: {value}')
             self.measurements[label] = value
-        if self.valueSaved == True:
-            self.checkMatching()
+            saved_value = self.savedValueLabels[label].text()
+            if saved_value:
+                saved_value = int(saved_value.split(": ")[1]) + 10          # since the range is (-10 - 10)
+                difference = abs(saved_value - (int(value)+ 10))            # adding 10 makes the cases easier to follow
+                if saved_value != 0:
+                    progress = int((1 - difference / abs(saved_value)) * 100)
+                else:
+                    progress = 100 if value == 0 else 0
+                #progress = int((1 - difference / saved_value) * 100)
+                self.progressBars[label].setValue(progress)
         self.lock = True
 
-    def checkMatching(self):
-        pass
+
+
     def freezeCalled(self):
-        while self.lock == False:
-            sleep(.1)
-            print("wait")
+        #while self.lock == False:
+         #   sleep(.1)
+          #  print("wait")
 
         self.lock = False
-        self.valueSaved = True
         for label, labelIndex in self.savedValueLabels.items():
             value = self.measurements[label] 
             labelIndex.setText(f'{label}: {value}')
@@ -108,5 +138,5 @@ class viewWindow(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = viewWindow()
+    window = ViewWindow()
     sys.exit(app.exec_())
