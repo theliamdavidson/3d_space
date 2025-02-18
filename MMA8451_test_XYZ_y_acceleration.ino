acbcbab702ec6de844c47668cf9ab389df267c3f
Liam Davidson <46986875+theliamdavidson@users.noreply.github.com>
2025-02-18 07:39:22 -0800

    removed commented out code

--- MMA8451_test_XYZ_y_acceleration.ino
+++ MMA8451_test_XYZ_y_acceleration.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
-
+                                  // testing a new accelerometer
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 static int loop_counter = 0;
 
@@ -16,19 +16,18 @@ void setup(void) {
     while (1);
   }
   
-  mma.setRange(MMA8451_RANGE_2_G);
-  
+  mma.setRange(MMA8451_RANGE_2_G);    // set the max range of values. lower ranges have higher resolution
   Serial.print("Range = "); Serial.print(2 << mma.getRange());  
   Serial.println("G");
 }
 
-int find_avg(int valueArray[]){       // we know the size of the array so passing it is simple
+int find_avg(int valueArray[]){       
   int avg = 0;
   for(int i = 0; i < 4; i++){
     avg += valueArray[i];
   }
   avg = avg/4.0;
-  avg += 0.5;                         // rounding up the number by adding .5 and flooring the result
+  avg += 0.5;                         // round up the number by adding .5 and floor the result
   avg = int(avg);
   return avg;
 }
@@ -36,17 +35,9 @@ int find_avg(int valueArray[]){       // we know the size of the array so passin
 void loop() {
   // Read the 'raw' data in 14-bit counts
   int myX[4];
-  //int myXr[4];
   int myY[4];
-  //int myYr[4];
   int myZ[4];
-  //int myZr[4];
   while (loop_counter < 4){
-    //mma.read();
-    //myXr[loop_counter] = mma.x;
-    //myYr[loop_counter] = mma.y;
-    //myZr[loop_counter] = mma.z;
-  
     sensors_event_t event; 
     mma.getEvent(&event);
 
